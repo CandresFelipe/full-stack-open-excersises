3d7c387c1203ff861a8882bd8ea41bc3bc5d1fb5
Andres Carrera <carrerafandres2496@gmail.com>
2024-09-17 11:34:15 +0200

    complete exercise 2.8 step 3

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,25 +2,39 @@ import { useState } from "react";
 import { Header } from "./components/Header";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
 
     if (persons.some((person) => person.name === newName)) {
       window.alert(`Person with name ${newName} already was added.`);
+      setNewName("");
       return;
     }
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
+    if (!(newPerson.name && newPerson.number)) {
+      window.alert("Inputs are not fully completed");
+      return;
+    }
     setPersons(persons.concat(newPerson));
     setNewName("");
+    setNewNumber("");
   };
 
   const onNameInputChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const onNumberInputChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -31,12 +45,17 @@ const App = () => {
           name: <input value={newName} onChange={onNameInputChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={onNumberInputChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
