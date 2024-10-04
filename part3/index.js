cb872132bd7537dcb1fad03600d81d2b808dbe9a
Andres Carrera <carrerafandres2496@gmail.com>
2024-10-04 12:41:46 +0200

    excersise 3.2 completed

--- part3/index.js
+++ part3/index.js
@@ -25,8 +25,6 @@ let persons = [
 
 const app = express()
 
-app.use(express.json())
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello world!@!!</h1>')
 })
@@ -35,43 +33,15 @@ app.get('/api/persons',(req, res) => {
     res.json(persons)
 })
 
-// app.get('/api/persons/:id', (req, res) => {
-//     const id = req.params.id
-//     const note = persons.find(note => note.id === id)
-//     if(!note) {
-//         res.status(404).send('<h1>Note was not found!</h1>').end()
-//     }
-//     res.json(note)
-// })
-
-// app.delete('/api/persons/:id', (req, res) => {
-//     const id = req.params.id
-//     notes = notes.filter((note) => note.id !== id)
-//     res.status(204).end()
-// })
-
-// app.post('/api/persons', (req, res) => {
-//   const body = req.body
-
-//   if(!body.content) {
-//     return res.status(400).json({
-//       error: 'Content missing'
-//     })
-//   }
-
-//   const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-
-//   newNote.id = String(maxId + 1)
-//   const newNote = {
-//     id: String(maxId + 1),
-//     content: body.content,
-//     important: Boolean(body.important) || false
-//   }
-//   notes = notes.concat(newNote)
-//   console.log(newNote)
-//   res.json(newNote)
-
-// })
+app.get('/info', (req, res) => {
+  const numOfPeople = persons.length
+  let time = undefined
+  if(req) {
+    time = new Date()
+  }
+  res.send(`<div><p>Phonebook has info for ${numOfPeople} people</p><p>${time}</p></div>`)
+  res.end()
+})
 
 const PORT = 3001
 
