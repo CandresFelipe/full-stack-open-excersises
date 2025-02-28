060e69e8a23c90595c04b6ce4edf6e9ad87fd543
Andres Carrera <carrerafandres2496@gmail.com>
2025-02-28 20:40:27 +0100

    complete exercises 6.2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const onClickButton = (event) => {
+    event.preventDefault();
+    if (event.target.innerText === "reset stats") {
+      store.dispatch({ type: "ZERO" });
+      return;
+    }
+    store.dispatch({ type: String(event.target.innerText).toUpperCase() });
+  };
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={onClickButton}>good</button>
+      <button onClick={onClickButton}>ok</button>
+      <button onClick={onClickButton}>bad</button>
+      <button onClick={onClickButton}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
