dc807b0130afba7c819fcaeb6e128e964a57028c
Andres Carrera <carrerafandres2496@gmail.com>
2025-07-22 09:43:43 +0200

    finish excersise 7.7

---
+++ part7/country-hook/src/hooks/useCountry.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const countryFetchURL = "https://studies.cs.helsinki.fi/restcountries/api/name";
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if(!name) return;
+
+    setLoading(true);
+    axios.get(`${countryFetchURL}/${name}`).then((data) => {
+        setCountry({
+            name: data.data.name.common,
+            capital: data.data.capital,
+            population: data.data.population,
+            flag: data.data.flags.svg,
+            found: true,
+        })
+    }).catch((error) => {
+        if(error.response.status === 404) {
+            setCountry({
+                ...country,
+                found: false,
+            })
+        }
+        console.log('Error Fetching country', error);
+    }).finally(() => {
+        setLoading(false)
+    })
+  }, [name])
+
+  return {
+    country,
+    loading,
+  }
+}
